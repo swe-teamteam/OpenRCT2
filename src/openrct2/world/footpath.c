97b7c98f1e24f257a3dffc0830fde41c9b78fa69
Chris <crorabaugh2015@my.fit.edu>
2017-03-23 12:39:10 -0400

    Fixed issue 5318, added ghost check to line 452 of footpath.c

--- src/openrct2/world/footpath.c
+++ src/openrct2/world/footpath.c
@@ -449,7 +449,7 @@ money32 footpath_remove_real(sint32 x, sint32 y, sint32 z, sint32 flags)
 		sub_6A759F();
 	}
 
-	return (flags & (1 << 5)) || (gParkFlags & PARK_FLAGS_NO_MONEY) ? 0 : -MONEY(10,00);
+	return (flags & (1 << 5)) || (gParkFlags & PARK_FLAGS_NO_MONEY) || !(flags & GAME_COMMAND_FLAG_GHOST) ? 0 : -MONEY(10,00);
 }
 
 /**
